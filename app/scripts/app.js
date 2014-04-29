c2f169d2b669a97a8382e441c19a6960ae1d7853
Maido Käära <maidokaara@gmail.com>
2014-04-29 20:22:54 +0100

    Added angular parse libs

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,17 +1,15 @@
-'use strict';
+(function (angular, Parse) {
+    'use strict';
 
-angular
-  .module('sladsApp', [
-    'ngSanitize',
-    'ngRoute'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
+    Parse.initialize('wHOusfpG1kv1wus81ORw8FNwW7V7NP3qXQMfSYdP', 'LNTVdhY79CqOwTC8xc1m6FVUN71WsrjFFYMsctuK');
+
+    angular.module('sladsApp', ['ngSanitize', 'ngRoute', 'mgcrea.ngStrap', 'parse-angular', 'parse-angular.enhance']).config(function ($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'views/main.html',
+            controller: 'MainCtrl'
+          }).otherwise({
+            redirectTo: '/'
+          });
       });
-  });
+
+  }(this.angular, this.Parse));
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,20 @@
-'use strict';
+(function (angular, Parse) {
+    'use strict';
 
-angular.module('sladsApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    var module = angular.module('sladsApp');
+
+    module.controller('MainCtrl', function ($scope) {
+        var Player = Parse.Object.extend({className: 'Player', attrs: ['firstName', 'lastName']});
+        var query = new Parse.Query(Player);
+        query.find().then(function (results) {
+            $scope.players = results;
+          });
+
+        var Match = Parse.Object.extend('Match');
+        var matchQuery = new Parse.Query(Match);
+        matchQuery.find().then(function (results) {
+            $scope.matches = results;
+          });
+      });
+
+  }(this.angular, this.Parse));
