3b646614da8fbd62782402d86cf37072b3e68271
Maido Käära <maidokaara@gmail.com>
2014-04-30 21:54:14 +0100

    Simple plaintext ordering

--- app/scripts/controllers/orders.js
+++ app/scripts/controllers/orders.js
@@ -9,11 +9,12 @@
 
     $scope.products = [];
 
-    var Order = Parse.Object.extend({className: 'Order', attrs: ['user', 'products']});
+    var Order = Parse.Object.extend({className: 'Order', attrs: ['user', 'content']});
+
+    var query = new Parse.Query(Order);
 
     var queryForOrders = function () {
-      var query = new Parse.Query(Order);
-      query.include('user', 'products');
+      query.include('user');
       query.find().then(function (results) {
         $scope.orders = results;
       });
@@ -21,67 +22,13 @@
 
     queryForOrders();
 
-    var Product = Parse.Object.extend({className: 'Product', attrs: ['name', 'price']});
-
-    var queryForProducts = function () {
-      var query = new Parse.Query(Product);
-      query.find().then(function (results) {
-        $scope.products = results;
-      });
-    };
-
-    queryForProducts();
-
     $scope.order = function () {
 
       var modalScope = $scope.$new();
       modalScope.title = 'Order';
 
       var fields = [
-      ];
-
-      modalScope.fields = fields;
-
-      var d = $modal({
-        template: 'views/modal.html',
-        scope: modalScope
-      });
-
-      modalScope.save = function () {
-        var m =  new Order();
-
-        m.set('user', parseService.currentUser());
-
-        var product = new Product();
-        product.set('name', 'Test2');
-        product.set('price', 2.0);
-        product.save(null, {
-          success: function () {
-            m.add('products', product);
-
-            m.save(null, {
-              success: function (){
-                queryForOrders();
-                d.hide();
-              }
-            });
-          }
-        });
-
-      };
-    };
-
-    $scope.openProductModal = function (product) {
-
-      var modalScope = $scope.$new();
-      modalScope.title = 'Product';
-
-      var name = product ? product.getName() : undefined;
-      var price = product ? product.getPrice() : undefined;
-
-      var fields = [
-        {label: 'Name', type: 'text', value: name, placeholder: 'Name'},
-        {label: 'Price', type: 'number', value: price, placeholder: 'Price'}
+        {label: 'Content', type: 'text', placeholder: 'Insert order as text'}
       ];
 
       modalScope.fields = fields;
@@ -92,17 +39,36 @@
       });
 
       modalScope.save = function (fields) {
-        var m = product || new Product();
-        m.set('name', fields[0].value);
-        m.set('price', fields[1].value);
+        var order = new Order();
 
-        m.save(null, {
+        order.set('user', parseService.currentUser());
+        order.set('content', fields[0].value);
+
+        order.save(null, {
           success: function (){
-            queryForProducts();
+            queryForOrders();
             d.hide();
           }
         });
       };
+    };
+
+    $scope.clear = function () {
+      query.find().then(function (results) {
+        var destroy = function (results) {
+          if(results.length > 0) {
+            var order = results.pop();
+            order.destroy({
+              success: function () {
+                destroy(results);
+              }
+            });
+          } else {
+            queryForOrders();
+          }
+        };
+        destroy(results);
+      });
     };
 
   });
