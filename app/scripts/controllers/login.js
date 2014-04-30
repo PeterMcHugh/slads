34f72401783392784bd866dce3715c2df6a0cb75
Maido Käära <maidokaara@gmail.com>
2014-04-30 22:30:42 +0100

    Corrected indentation

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -1,27 +1,27 @@
 (function (angular) {
-    'use strict';
+  'use strict';
 
-    var module = angular.module('sladsApp');
+  var module = angular.module('sladsApp');
 
-    module.controller('LoginCtrl', function ($scope, $location, parseService) {
+  module.controller('LoginCtrl', function ($scope, $location, parseService) {
 
-        $scope.formError = '';
-        
-        if (parseService.currentUser() !== null) {
+    $scope.formError = '';
+
+    if (parseService.currentUser() !== null) {
+      $location.path('/main');
+    }
+
+    $scope.login = function (username, password) {
+      parseService.login(username, password, {
+        success: function () {
+          $scope.formError = '';
           $location.path('/main');
+        },
+        error: function (user, error) {
+          $scope.formError = error.message;
         }
-
-        $scope.login = function (username, password) {
-            parseService.login(username, password, {
-                success: function () {
-                  $scope.formError = '';
-                  $location.path('/main');
-                },
-                error: function (user, error) {
-                  $scope.formError = error.message;
-                }
-              });
-          };
       });
+    };
+  });
 
-  }(this.angular));
+}(this.angular));
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,22 +1,22 @@
 (function (angular) {
-    'use strict';
+  'use strict';
 
-    var module = angular.module('sladsApp');
+  var module = angular.module('sladsApp');
 
-    module.controller('MainCtrl', function ($scope, parseService) {
-        
-      $scope.currentUser = parseService.currentUser();
-      
-      $scope.page = 'ladder';
-      
-      $scope.logout = function () {
-        parseService.logout();
-      };
-      
-      $scope.showPage = function(page) {
-        $scope.page = page;
-      };
-      
-    });
+  module.controller('MainCtrl', function ($scope, parseService) {
 
-  }(this.angular));
+    $scope.currentUser = parseService.currentUser();
+
+    $scope.page = 'ladder';
+
+    $scope.logout = function () {
+      parseService.logout();
+    };
+
+    $scope.showPage = function(page) {
+      $scope.page = page;
+    };
+
+  });
+
+}(this.angular));
